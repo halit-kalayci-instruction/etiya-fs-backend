21506f0096377c5f622c9d64e947d3a6d61378ed
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-12-12 10:43:16 +0300

    db connection

--- src/main/java/com/etiya/training/entities/Category.java
+++ src/main/java/com/etiya/training/entities/Category.java
@@ -1,14 +1,20 @@
 package com.etiya.training.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Table(name="categories")
 @Entity
+@Data // getter-setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
     @Column(name="category_id")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int categoryId;
+    private Short categoryId;
 
     @Column(name="category_name")
     private String categoryName;
